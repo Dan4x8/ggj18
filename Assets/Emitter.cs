e4079d8bf0e3d1aec1f11de8efdeb35da750be70
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2018-01-26 23:08:17 +0100

    basic implementation emmiter.cs

---
+++ Assets/Emitter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+    public AudioSource AudioSource;
+    private float _range;
+
+	// Use this for initialization
+	void Start () {
+        _range = AudioSource.maxDistance;
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+
+    }
+
+    private void OnCollisionExit(Collision c)
+    {
+
+    }
+}
