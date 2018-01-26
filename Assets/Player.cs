1d7f2cf4bc4029838ac1bc0dae839d8852514469
Daniel Schütz <danielschuetz@gmx.ch>
2018-01-26 22:46:50 +0100

    added Player Class incl. Register/Unregister Emitter

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private List<Transform> _emitterCollection;
+
+	private List<Transform> EmitterCollection
+	{
+		get { return _emitterCollection; }
+	}
+
+	private void Start()
+	{
+		_emitterCollection = new List<Transform>();
+	}
+
+	public void RegisterEmitter(GameObject sender)
+	{
+		if (!EmitterCollection.Contains(sender.transform))
+			EmitterCollection.Add(sender.transform);
+	}
+
+	public void UnregisterEmitter(GameObject sender)
+	{
+		EmitterCollection.Remove(sender.transform);
+	}
+}
