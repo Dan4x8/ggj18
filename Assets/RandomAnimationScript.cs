b26750bae14353a080dc2b5ee3887b4c4300d825
Daniel Schütz <danielschuetz@gmx.ch>
2018-01-28 00:13:46 +0100

    updatet transmitter

---
+++ Assets/RandomAnimationScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RandomAnimationScript : MonoBehaviour
+{
+	public List<AnimationClip> AnimationCollection = new List<AnimationClip>();
+	public bool RandomRotation = true;
+
+	private void Start()
+	{
+
+		var r = Random.Range(0, AnimationCollection.Count);
+		var aoc = new AnimatorOverrideController(GetComponent<Animator>().runtimeAnimatorController);
+		var anims = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+		
+		anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(aoc.animationClips[0], AnimationCollection[r]));
+		aoc.ApplyOverrides(anims);
+		GetComponent<Animator>().runtimeAnimatorController = aoc;
+		if (RandomRotation)
+		{
+			transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+		}
+
+	}
+}
