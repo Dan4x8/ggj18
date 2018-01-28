5c6a91fe0a43c125f70bd4e626685dabb21001b1
Daniel Schütz <danielschuetz@gmx.ch>
2018-01-28 01:30:29 +0100

    changed value

--- Assets/Wall/Wall.cs
+++ Assets/Wall/Wall.cs
@@ -19,7 +19,7 @@ public class Wall : MonoBehaviour
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		var p = collision.collider.GetComponent<Player>();
-		var off = p.CalculateDirection()*-1f;
+		var off = p.CalculateDirection()*-1.5f;
 		p.transform.position += off.normalized;
 	}
 }
