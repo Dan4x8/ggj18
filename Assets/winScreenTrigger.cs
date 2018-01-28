18c283b11ac4d0f75a7a6b88363943936d4f6c47
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2018-01-28 16:06:45 +0100

    win trigger

---
+++ Assets/winScreenTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winScreenTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Win");
+	}
+}
